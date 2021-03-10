5659da564a72529a249cc9eab2f65238eee59e68
JamesHarcourt7 <jh3g20@soton.ac.uk>
2021-03-10 01:36:30 +0000

    Created rough prototype and fixed bugs in program.

--- flat-waste-management.ino
+++ flat-waste-management.ino
@@ -10,19 +10,23 @@ const int YELLOW_LED = 5;
 const int GREEN_LED = 6;
 
 // Timer variables. There are 172800 seconds in 48 hours.
-const int total_seconds = 30
+const int total_seconds = 30;
 int seconds; 
 int seconds_over = 0;
 bool halfway;
 bool finished;
 
 // Define the array that decides who's turn it is to go (order generated randomly beforehand).
-String[] bin_takers = new String[] {"James W", "Immy", "Mya", "James H", "Louis", "Liv", "Carl", "Lucy", "Sam", "Anna"};
+String bin_takers[10] = {"James W", "Immy", "Mya", "James H", "Louis", "Liv", "Carl", "Lucy", "Sam", "Anna"};
 int index;
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 void setup() {
+  // DEBUGGING REMOVE LATER
+  Serial.begin(115200);
+  Serial.print("HELLOOOO");
+  
   // Set number of rows and columns in LCD.
   lcd.begin(16, 2);
   delay(1000);
@@ -32,11 +36,12 @@ void setup() {
   halfway = false;
   finished = false;
 
-  // Set up the LEDS and button.
+  // Set up the LEDS, buzzer and button.
   digitalWrite(RED_LED, LOW);
   digitalWrite(YELLOW_LED, LOW);
   digitalWrite(GREEN_LED, HIGH);
-  pinMode(BUTTON_LED, INPUT_PULLUP);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
 
   // Get the index from EEPROM if its there, otherwise make it again.
   // REMEMBER TO ADD THAT BTW I KNOW YOUR BEING LAZY RN.
@@ -48,40 +53,45 @@ void loop() {
   if (!finished) {
     // Decrement the timer.
     seconds -= 1;
+    Serial.println(seconds);
 
-    if (seconds == 0) {
+    if (seconds <= 0) {
+      seconds = 0;
       finished = true;
+      Serial.println("RED");
       digitalWrite(RED_LED, HIGH);
       digitalWrite(YELLOW_LED, LOW);
       digitalWrite(GREEN_LED, LOW);
 
-      setCursor(0, 1);
-      print("It is " + bin_takers[index] + "'s");
-      setCursor(0, 2);
-      print("turn to take bin");
+      lcd.setCursor(0, 1);
+      lcd.print("It is " + bin_takers[index] + "'s");
+      lcd.setCursor(0, 2);
+      lcd.print("turn to take bin");
        
-    } else if (seconds <= total_seconds / 2) {
-      halway = true;
+    } else if (!halfway && seconds <= total_seconds / 2) {
+      halfway = true;
       digitalWrite(RED_LED, LOW);
       digitalWrite(YELLOW_LED, HIGH);
       digitalWrite(GREEN_LED, LOW);
+      Serial.println("YELLOW");
     }
     
-  else {
+  } else {
     // Keep track of seconds gone over, to prevent the alarm trigger shifting into unreasonable hours of the night.
     seconds_over ++;
     // Play the note C5 through pin D8 for 200ms.
-    tone(8, NOTE_C5, 200);
+    tone(BUZZER_PIN, NOTE_C5, 200);
     
     // If the timer has finished, check for button press to reset.
-    if (digitalRead(BUTTON_PIN, LOW) {
-      seconds = seconds_over;
+    if (digitalRead(BUTTON_PIN) == LOW) {
+      seconds = total_seconds - seconds_over;
       seconds_over = 0;
       finished = false;
       halfway = false;
       digitalWrite(RED_LED, LOW);
       digitalWrite(YELLOW_LED, LOW);
       digitalWrite(GREEN_LED, HIGH);
+      Serial.println("GREEN");
     }
   }
   
